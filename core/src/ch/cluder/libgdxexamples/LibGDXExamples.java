2674c5ba0ed0bf66ad63c1fccadcea1b6ae40c0b
cluder <cluder@users.noreply.github.com>
2019-09-01 15:54:59 +0200

    Initial commit

---
+++ core/src/ch/cluder/libgdxexamples/LibGDXExamples.java
@@ -0,0 +1,100 @@
+package ch.cluder.libgdxexamples;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
+public class LibGDXExamples extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	public PerspectiveCamera cam;
+	public CameraInputController inputController;
+
+	// axes / grid (from BaseG3dTest)
+	final float GRID_MIN = -10f;
+	final float GRID_MAX = 10f;
+	final float GRID_STEP = 1f;
+	public Model axesModel;
+	public ModelInstance axesInstance;
+	public ModelBatch modelBatch;
+
+	@Override
+	public void create() {
+		modelBatch = new ModelBatch();
+
+		// create a 3d camera
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 0.1f;
+		cam.far = 1000f;
+		cam.update();
+
+		// create a camera controller
+		Gdx.input.setInputProcessor(inputController = new CameraInputController(cam));
+
+		batch = new SpriteBatch();
+		img = new Texture("text.png");
+
+		createAxes();
+	}
+
+	@Override
+	public void render() {
+		// black background
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		batch.begin();
+		// draw text in upper left corner
+		batch.draw(img, 0, Gdx.app.getGraphics().getHeight() - img.getHeight());
+		batch.end();
+
+		modelBatch.begin(cam);
+		modelBatch.render(axesInstance);
+		modelBatch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		img.dispose();
+	}
+
+	/**
+	 * Axses / Grid copied from BaseG3dTest.
+	 */
+	private void createAxes() {
+		ModelBuilder modelBuilder = new ModelBuilder();
+		modelBuilder.begin();
+		MeshPartBuilder builder = modelBuilder.part("grid", GL20.GL_LINES, Usage.Position | Usage.ColorUnpacked,
+				new Material());
+		builder.setColor(Color.LIGHT_GRAY);
+		for (float t = GRID_MIN; t <= GRID_MAX; t += GRID_STEP) {
+			builder.line(t, 0, GRID_MIN, t, 0, GRID_MAX);
+			builder.line(GRID_MIN, 0, t, GRID_MAX, 0, t);
+		}
+		builder = modelBuilder.part("axes", GL20.GL_LINES, Usage.Position | Usage.ColorUnpacked, new Material());
+		builder.setColor(Color.RED);
+		builder.line(0, 0, 0, 100, 0, 0);
+		builder.setColor(Color.GREEN);
+		builder.line(0, 0, 0, 0, 100, 0);
+		builder.setColor(Color.BLUE);
+		builder.line(0, 0, 0, 0, 0, 100);
+		axesModel = modelBuilder.end();
+		axesInstance = new ModelInstance(axesModel);
+	}
+}
